d1c7334e8ac446752b2dcebf116c4f8710531862
chan <jincan39@163.com>
2024-11-12 10:11:12 +0800

    fix: rm walletconnect-connector & solana-connector

--- packages/connectors/src/defaultConnectors.ts
+++ packages/connectors/src/defaultConnectors.ts
@@ -6,9 +6,6 @@ import {
 import { FuelWalletDevelopmentConnector } from '@fuel-connectors/fuel-development-wallet';
 import { FuelWalletConnector } from '@fuel-connectors/fuel-wallet';
 import { FueletWalletConnector } from '@fuel-connectors/fuelet-wallet';
-import { SolanaConnector } from '@fuel-connectors/solana-connector';
-import { WalletConnectConnector } from '@fuel-connectors/walletconnect-connector';
-import { type Config, connect } from '@wagmi/core';
 import type { ProviderType } from '@web3modal/solana/dist/types/src/utils/scaffold';
 import type { FuelConfig, FuelConnector } from 'fuels';
 import type { Provider as FuelProvider } from 'fuels';
@@ -17,20 +14,13 @@ type DefaultConnectors = {
   devMode?: boolean;
   wcProjectId?: string;
   burnerWalletConfig?: BurnerWalletConfig;
-  ethWagmiConfig?: Config;
-  ethSkipAutoReconnect?: boolean;
-  solanaConfig?: ProviderType;
   chainId?: number;
   fuelProvider?: FuelProvider | Promise<FuelProvider>;
 };
 
 export function defaultConnectors({
   devMode,
-  wcProjectId,
   burnerWalletConfig,
-  ethWagmiConfig,
-  ethSkipAutoReconnect,
-  solanaConfig: _solanaConfig,
   chainId,
   fuelProvider,
 }: DefaultConnectors = {}): Array<FuelConnector> {
@@ -38,18 +28,6 @@ export function defaultConnectors({
     new FuelWalletConnector(),
     new BakoSafeConnector(),
     new FueletWalletConnector(),
-    new WalletConnectConnector({
-      projectId: wcProjectId,
-      wagmiConfig: ethWagmiConfig,
-      chainId,
-      fuelProvider,
-      skipAutoReconnect: ethSkipAutoReconnect,
-    }),
-    new SolanaConnector({
-      projectId: wcProjectId,
-      chainId,
-      fuelProvider,
-    }),
   ];
 
   if (devMode) {
--- packages/connectors/src/index.ts
+++ packages/connectors/src/index.ts
@@ -5,5 +5,3 @@ export * from '@fuel-connectors/fuel-wallet';
 export * from '@fuel-connectors/fuelet-wallet';
 export * from '@fuel-connectors/bako-safe';
 export * from '@fuel-connectors/burner-wallet-connector';
-export * from '@fuel-connectors/walletconnect-connector';
-export * from '@fuel-connectors/solana-connector';
